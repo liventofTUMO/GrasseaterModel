91033712be591ad8311f47e9b06fda1d61e79201
liventofTUMO <livent@tumo.world>
2023-06-03 12:30:32 +0200

    Text z

---
+++ Server-Game/test.js
@@ -0,0 +1,12 @@
+let object = {
+    first_name = "Liam",
+    last_name = "Wiren-Chapel",
+
+    sayHello(){
+        console.log("Hello There!")
+    }
+}
+
+console.log(object);
+console.log(object.last_name);
+object.sayHello();
