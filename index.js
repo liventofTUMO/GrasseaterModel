3f8cda09aa93b967d1a48777a53008eb851da6e0
liventofTUMO <livent@tumo.world>
2023-06-14 18:16:06 +0200

    -

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-console.log("Hallo nodJS...");
-const os = require("os");
-let message = "Die Platform ist ";
+// console.log("Hallo nodJS...");
+// const os = require("os");
+// let message = "Die Platform ist ";
 
+// function main(){
+//     console.log(message + os.platform())
+// }
+// main()
+
+****************
+const Square = require("./module")
 function main(){
-    console.log(message + os.platform())
+    let Square = new Square(50)
+    console.log(square.getArea())
 }
-main()
